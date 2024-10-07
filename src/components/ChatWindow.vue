49a0c8102a264c0b972eeb5e1500bf76b57e48c1
zhassulaan <serikuly.zhassulan@gmail.com>
2024-10-07 10:23:08 +0500

    upd

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="chat_window">
     <span class="chat_window-header">Chat with <b>{{ selected_chat }}</b></span>
+
     <div class="chat_window-messages">
       <div
         class="chat_window-message"
@@ -14,6 +15,7 @@
         <p>{{ message.text }}</p>
       </div>
     </div>
+
     <div class="chat_window-controller">
       <input v-model="new_message" placeholder="Type your message..."  class="chat_window-controller-input"/>
       <button @click="send_message" class="chat_window-controller-button">↑</button>
@@ -35,9 +37,8 @@ const emit = defineEmits(['load_messages']);
 function save_message(message) {
   let chat_history = JSON.parse(localStorage.getItem('chat_messages')) || {};
   const chat_key = [props.current_user, props.selected_chat].sort().join('_');
-  if (!chat_history[chat_key]) {
+  if (!chat_history[chat_key])
     chat_history[chat_key] = [];
-  }
   chat_history[chat_key].push(message);
   localStorage.setItem('chat_messages', JSON.stringify(chat_history));
   emit('load_messages');
@@ -65,65 +66,64 @@ function send_message() {
     position: inherit;
   }
   &-header {
+    border-left: 1px solid var(--clr-grey);
     width: 100%;
     padding: 20px;
-    border-left: 1px solid var(--clr-grey);
     background-color: var(--clr-white);
   }
   &-messages,
   &-controller {
     left: 50%;
     transform: translate(-50%);
+    width: 60%;
   }
   &-messages {
     bottom: 64px;
     overflow-y: auto;
-    width: 60%;
   }
   &-message {
     max-width: 60%;
     padding: 10px;
     margin: 5px;
     border-radius: 10px;
-  }
-  &-message_right {
-    text-align: right;
-    background-color: var(--primary-chat-color);
-    margin-left: auto;
-  }
-  &-message_left {
-    text-align: left;
-    background-color: var(--secondary-chat-color);
-    margin-right: auto;
+    &_right {
+      text-align: right;
+      background-color: var(--primary-chat-color);
+      margin-left: auto;
+    }
+    &_left {
+      margin-right: auto;
+      background-color: var(--secondary-chat-color);
+      text-align: left;
+    }
   }
   &-controller {
     bottom: 0;
-    width: 60%;
+    overflow: hidden;
     border-radius: 18px;
     background-color: var(--clr-white);
-    overflow: hidden;
-  }
-  &-controller-input {
-    width: 100%;
-    padding: 12px 42px 12px 10px;
-    border: 0;
-  }
-  &-controller-button {
-    position: absolute;
-    right: 5px;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 32px;
-    height: 32px;
-    background-color: #0088CC;
-    color: var(--clr-white);
-    border: none;
-    border-radius: 50%;
-    cursor: pointer;
-    transition: background-color 0.3s;
-  }
-  &-controller-button:hover {
-    background-color: #006699;
+    &-input {
+      border: 0;
+      width: 100%;
+      padding: 12px 42px 12px 10px;
+    }
+    &-button {
+      position: absolute;
+      top: 50%;
+      right: 5px;
+      transform: translateY(-50%);
+      transition: background-color 0.3s;
+      border: none;
+      border-radius: 50%;
+      width: 32px;
+      height: 32px;
+      background-color: #0088CC;
+      cursor: pointer;
+      color: var(--clr-white);
+      &:hover {
+        background-color: #006699;
+      }
+    }
   }
 }
 </style>
